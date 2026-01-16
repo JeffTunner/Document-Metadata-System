f50b14b9c12b0b86f351d149f0b5c74d5cd182e7
JeffTunner <abhinavkaushik05@gmail.com>
2026-01-16 15:22:48 +0530

    Initial Commit

---
+++ src/test/java/com/example/documentMetaData/DocumentMetaDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.documentMetaData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentMetaDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
